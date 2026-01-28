7def61a857de17cb40dd70a025902dab8c12f341
David Valek <david.valek17@gmail.com>
2026-01-28 11:50:35 +0100

    Update home links to work in gh pages

--- app/pages/history/index.vue
+++ app/pages/history/index.vue
@@ -5,7 +5,7 @@
       <div class="card history-card">
         <div class="top-nav">
           <h1>Vaše nedávná vyhledávání</h1>
-          <a class="back-btn" href="/">
+          <a class="back-btn" href="/krigsbyte/">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
--- app/pages/record.vue
+++ app/pages/record.vue
@@ -38,7 +38,7 @@
                 <rect x="6" y="14" width="12" height="8" />
               </svg>
             </button>
-            <router-link class="back-btn" to="/">
+            <router-link class="back-btn" to="/krigsbyte/">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
